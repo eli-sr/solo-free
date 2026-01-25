6ea0b9c702481442c1761e27d31a17dff9b7437f
eli <elzr.sr@gmail.com>
2026-01-25 18:20:15 +0100

    feat: Add accent-insensitive filtering

--- src/utils/wordUtils.ts
+++ src/utils/wordUtils.ts
@@ -3,9 +3,17 @@ export interface FilterOptions {
   filterString?: string;
 }
 
+const normalizeString = (str: string): string => {
+  return str.normalize("NFD").replace(/[\u0300-\u0302\u0304-\u036f]/g, "").normalize("NFC");
+}
+
 export const filterWords = (words: string[], filterString?: string): string[] => {
-    filterString = filterString?.replace(/\*/g, '[bcdfghjklmnñpqrstvwxyz]*')
-    const regexString = ".*".concat(filterString || "").concat("$")
+    if (!filterString) return words
+    
+    const normalizedFilter = normalizeString(filterString).replace(/\*/g, '[bcdfghjklmnñpqrstvwxyz]*')
+    
+    const regexString = ".*".concat(normalizedFilter).concat("$")
     const regex = new RegExp(regexString)
-    return words.filter(word => regex.test(word))
+    
+    return words.filter(word => regex.test(normalizeString(word)))
 };
